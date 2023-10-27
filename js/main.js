6a95c2d1cb6339d89bdc87711be11eac1ca8d98b
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-10-27 15:43:21 -0600

    finished project - clone spotify

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+/* Funcionality of menu logo of navbar - responsiveness*/
+
 const menuIcon = document.querySelector('.menu-icon');
 const navLinks = document.querySelector('nav ul');
 
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+});
+
+
+
+/* for movement of header*/
+const header = document.querySelector('header');
+
+
+window.addEventListener('scroll', function() {
+
+    const scrollPosition = window.scrollY || window.pageYOffset;
+
+    if (scrollPosition > 0) {
+        header.classList.add('scrolling');
+    } else {
+        
+        header.classList.remove('scrolling');
+    }
 });
